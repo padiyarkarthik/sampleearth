7cf971e25a37abb463c55849346728a2059735a6
desham90 <s.dere.05@gmail.com>
2018-06-14 08:03:05 +0530

    updated output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask,redirect,render_template,request
 import pypyodbc
 import time
+import random
 app = Flask(__name__)
 
 def disdata():
@@ -18,13 +19,37 @@ def disdata():
    executiontime = end - start
    return render_template('searchearth.html', ci=row, t=executiontime)
 
+def randrange(numofqueries=None,rangefrom=None,rangeto=None):
+   server = 'sham05.database.windows.net'
+   database = 'sqldb'
+   username = 'sham05'
+   password = '1qaz!QAZ'
+   driver= '{ODBC Driver 13 for SQL Server}'
+   cnxn = pypyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+   cursor = cnxn.cursor()
+   start = time.time()
+   for x in range(5):
+       magnitude = round(random.uniform(float(rangefrom),float(rangeto)),2)
+       sqlNum = "SELECT * FROM [earth_data] where mag="+str(magnitude)
+       cursor.execute(sqlNum)
+   end = time.time()
+   executiontime = end - start
+   return render_template('count.html',t=executiontime)
+
 @app.route('/')
 def hello_world():
   return render_template('index.html')
 
 @app.route('/displaydata', methods=['POST'])
 def display():
-    return disdata()  
+    return disdata() 
+
+@app.route('/multiplerun', methods=['GET'])
+def randquery():
+    rangfro = float(request.args.get('rangefrom'))
+    rangto = float(request.args.get('rangeto'))
+    numqueries = request.args.get('numqueries')
+    return randrange(numqueries,rangfro,rangto) 	
 
 if __name__ == '__main__':
   app.run()
