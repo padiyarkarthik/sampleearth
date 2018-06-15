6e93bcfefece6faa3062b68fe9afc31272bf8e4d
padiyarkarthik <karthik.padiyar@mavs.uta.edu>
2018-06-14 19:07:06 -0500

    Hi

--- main.py
+++ main.py
@@ -2,41 +2,30 @@ import os
 from flask import Flask,redirect,render_template,request
 import pypyodbc
 import time
-import random
+#import random
 import urllib
 import datetime
-import json
+#import json
+#import redis
 
 app = Flask(__name__)
 
 
-server = 'sham05.database.windows.net'
-database = 'sqldb'
-username = 'sham05'
-password = '1qaz!QAZ'
+server = 'kpmaster.database.windows.net'
+database = 'kpmaster'
+username = 'kpmaster'
+password = 'karu@1965'
 driver= '{ODBC Driver 13 for SQL Server}'
    
 def disdata():
    cnxn = pypyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
    cursor = cnxn.cursor()
    start = time.time()
-   cursor.execute("SELECT TOP 10000 * FROM [earth_data]")
+   cursor.execute("SELECT TOP 10 * FROM [equake]")
    row = cursor.fetchall()
    end = time.time()
    executiontime = end - start
    return render_template('searchearth.html', ci=row, t=executiontime)
-
-def randrange(rangfro=None,rangto=None,num=None):
-    dbconn = pypyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = dbconn.cursor()
-    start = time.time()
-    for i in range(0,int(num)):
-    	mag= round(random.uniform(rangfro, rangto),2)
-    	success="SELECT * from [earth_data] where mag>'"+str(mag)+"'"
-    	cursor.execute(success)
-    end = time.time()
-    exectime = end - start
-    return render_template('count.html', t=exectime)
 
 @app.route('/')
 def hello_world():
@@ -45,13 +34,6 @@ def hello_world():
 @app.route('/displaydata', methods=['POST'])
 def display():
     return disdata() 
-
-@app.route('/multiplerun', methods=['GET'])
-def randquery():
-    rangfro = float(request.args.get('rangefrom'))
-    rangto = float(request.args.get('rangeto'))
-    num = request.args.get('nom')
-    return randrange(rangfro,rangto,num) 	
 
 if __name__ == '__main__':
   app.run()
